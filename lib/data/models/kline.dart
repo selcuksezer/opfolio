bea8393839b07fffc61f76cfcc74aa4e0c46fa88
selcuksezer <selcuksezer6@gmail.com>
2022-05-03 23:51:35 +0300

    add kline model

---
+++ lib/data/models/kline.dart
@@ -0,0 +1,17 @@
+class Kline {
+  final double open;
+  final double high;
+  final double low;
+  final double close;
+  final double volume;
+  final int timestamp;
+
+  Kline({
+    required this.open,
+    required this.high,
+    required this.low,
+    required this.close,
+    required this.volume,
+    required this.timestamp,
+  });
+}
